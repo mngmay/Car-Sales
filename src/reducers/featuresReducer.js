e9b915a1e0c8f92602433cb156e9118c84d11a8d
May Ng <mngmay@gmail.com>
2019-08-14 12:51:41 -0700

    Added featuresActions. Wired buyItem function. Features can be added. When added, removes from store.

--- src/reducers/featuresReducer.js
+++ src/reducers/featuresReducer.js
@@ -1,7 +1,39 @@
-export const initialState = {};
+export const initialState = {
+  additionalPrice: 0,
+  car: {
+    price: 26395,
+    name: "2019 Ford Mustang",
+    image:
+      "https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg",
+    features: []
+  },
+  store: [
+    { id: 1, name: "V-6 engine", price: 1500 },
+    { id: 2, name: "Racing detail package", price: 1500 },
+    { id: 3, name: "Premium sound system", price: 500 },
+    { id: 4, name: "Rear spoiler", price: 250 }
+  ]
+};
+
+const ADD_FEATURE = "ADD_FEATURE";
+const REMOVE_FEATURE = "REMOVE_FEATURE";
 
 export const featuresReducer = (state = initialState, action) => {
   switch (action.type) {
+    case ADD_FEATURE:
+      return {
+        ...state,
+        car: {
+          ...state.car,
+          features: [...state.car.features, action.payload]
+        },
+        store: state.store.filter(ft => action.payload.id !== ft.id)
+      };
+    case REMOVE_FEATURE:
+      return {
+        ...state,
+        features: state.features.filter(ft => action.payload !== ft)
+      };
     default:
       return state;
   }
